3221d2adc38ca2001b0643cde348c205c3cb0405
Myles Moylan <moylanm@oregonstate.edu>
2022-12-08 18:44:58 -0800

    refactor functions to reside in Cipher class

--- cryptography/shift_cipher.py
+++ cryptography/shift_cipher.py
@@ -1,32 +1,39 @@
 # coding: utf-8
 
-import argparse, sys
+import argparse, sys, operator
 
-CHAR_TO_NUM = {
-  'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10,
-  'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20,
-  'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25
-}
-
-NUM_TO_CHAR = {
-  0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K',
-  11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U',
-  21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z',
-}
-
-MODULUS = 26
-
-def translate_characters(s: str) -> list:
-  return [CHAR_TO_NUM[x.upper()] for x in list(s)]
-
-def translate_numbers(n: list) -> list:
-  return [NUM_TO_CHAR[x] for x in n]
-
-def encrypt(p: int, k: int) -> int:
-  return (p + k) % MODULUS
-
-def decrypt(p: int, k: int) -> int:
-  return (p - k) % MODULUS
+class Cipher:
+  
+  CHAR_TO_NUM = {
+    'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10,
+    'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20,
+    'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25
+  }
+  
+  NUM_TO_CHAR = {
+    0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K',
+    11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U',
+    21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'
+  }
+  
+  def __init__(self, key: int, modulus: int = 26) -> None:
+    self.modulus = modulus
+    self.key = key
+  
+  def encrypt(self, text: str):
+    return self._run(operator.pos, text)
+  
+  def decrypt(self, text: str):
+    return self._run(operator.neg, text)
+  
+  def _run(self, op, text: str):
+    translation = [self.CHAR_TO_NUM[c.upper()] if c != ' ' else c for c in list(text)]
+    
+    for i in range(len(translation)):
+      if isinstance(translation[i], int):
+        translation[i] = self.NUM_TO_CHAR[(translation[i] + op(self.key)) % self.modulus]
+         
+    return ''.join(translation) # type: ignore
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -43,10 +50,7 @@ if __name__ == '__main__':
     print('Must choose to encrypt or decrpyt.')
     sys.exit()
     
-  result = []
-  action = encrypt if args.encrypt else decrypt
-
-  for s in args.string:
-    result.append([action(p, args.key) for p in translate_characters(s)])
+  cipher = Cipher(args.key)
+  action = 'encrypt' if args.encrypt else 'decrypt'
   
-  print(' '.join([''.join(translate_numbers(c)) for c in result]))
+  print(getattr(cipher, action)(' '.join(args.string)))
